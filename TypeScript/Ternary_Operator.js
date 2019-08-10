46baae5ce7ccd19d87761af6abcafcf771da2785
unknown <shoaibnayeem97@gmail.com>
2019-08-10 20:28:49 +0530

    Commited Ternary_Operator TypeScript files

---
+++ TypeScript/Ternary_Operator.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20;
+console.log(a > b ? "Value of a is greater than b" : "Values of a is less then b");
+function fun() {
+    return 1;
+}
+function Sum(x, y) {
+    return x + y;
+}
+console.log(Sum(1, 2));
+console.log(fun());
+var aVal = "Hello";
+var bVal = "Helo";
+console.log(aVal == bVal ? "Strings are equal" : "String are not eqaul");
