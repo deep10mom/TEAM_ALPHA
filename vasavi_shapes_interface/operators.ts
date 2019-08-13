aab25cada064e8f61e38076e27d319bfc8722c6b
Vasavikatakam <30796075+Vasavikatakam@users.noreply.github.com>
2019-08-12 20:45:11 -0700

    Add files via upload

---
+++ vasavi_shapes_interface/operators.ts
@@ -0,0 +1,14 @@
+var x:number =2;
+console.log(++x);
+console.log(x);
+console.log(x++);
+console.log(x);
+
+
+
+//--------------Ternary operator example -------------------------------
+
+var num:number =25;
+var result:string;
+result=(num<25)?"young":"old";
+console.log(result);
