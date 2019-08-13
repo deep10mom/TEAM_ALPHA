1c2e55c6cad0107f2d9017f7b39558b39a75afc9
shoaib-nayeem <53897836+shoaib-nayeem@users.noreply.github.com>
2019-08-13 22:18:52 +0530

    Add files via upload

---
+++ Shoaib/TypeScript/Ternary_Operator.ts
@@ -0,0 +1,17 @@
+
+
+var a:number=10;
+var b:number=20;
+console.log(a>b? "Value of a is greater than b":"Values of a is less then b");
+function fun() : number
+{
+    return 1;
+}
+function Sum(x: number, y: number) : number {
+    return x + y;
+}
+console.log(Sum(1,2));
+console.log(fun());
+let aVal:string="Hello";
+let bVal:string="Hello";
+console.log(aVal==bVal ? "Strings are equal": "String are not eqaul");
