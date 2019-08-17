88b17ff0da35075a7693cf6ab2db9b68500e6ff7
shoaib-nayeem <53897836+shoaib-nayeem@users.noreply.github.com>
2019-08-17 17:10:48 +0530

    Add files via upload

---
+++ Shoaib/ng-model-demo/ng-model-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-model-demo',
+  templateUrl: './ng-model-demo.component.html',
+  styleUrls: ['./ng-model-demo.component.css']
+})
+export class NgModelDemoComponent implements OnInit {
+
+  constructor() { }
+ CompanyName:string;
+  ngOnInit() {
+  }
+
+}
